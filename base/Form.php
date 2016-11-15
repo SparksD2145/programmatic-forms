96fcfc8eefac0fb9f9e18435033889d3e2c827c5
Thomas Ibarra <sparksd2145@gmail.com>
2016-11-15 10:08:18 -0600

    refactor(Form): remove guid

--- base/Form.php
+++ base/Form.php
@@ -5,6 +5,7 @@ namespace pgforms {
         private $configuration = [
             "items" => null,
             "name" => null,
+            "id" => null,
             "class" => null,
             "accept" => null,
             "action" => null,
@@ -12,25 +13,6 @@ namespace pgforms {
             "autorender" => true,
             "newline" => true
         ];
-
-        // Generate a Globally Unique ID
-        private static function GUID () {
-            if (function_exists('com_create_guid')){
-                return trim(com_create_guid(), '{}');
-            }else{
-                mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-                $charid = strtoupper(md5(uniqid(rand(), true)));
-                $hyphen = chr(45);// "-"
-                $uuid = chr(123)// "{"
-                    .substr($charid, 0, 8).$hyphen
-                    .substr($charid, 8, 4).$hyphen
-                    .substr($charid,12, 4).$hyphen
-                    .substr($charid,16, 4).$hyphen
-                    .substr($charid,20,12)
-                    .chr(125);// "}"
-                return $uuid;
-            }
-        }
 
         // Config keys to ignore during a render
         private static $render_ignore_keys = ["items", "autorender", "newline"];
@@ -54,9 +36,6 @@ namespace pgforms {
 
             // Open form
             $builder = "<form ";
-
-            // Assign ID
-            $builder .= "id='". self::GUID() ."' ";
 
             // Clone config to remove items from the list.
             $config = array_merge([], $this->configuration);
