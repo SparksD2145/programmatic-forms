a5212935ba6f07261f9f48454d82c3579b2cc8d3
Thomas Ibarra <tibarra@nobilishealth.com>
2016-12-20 12:44:35 -0600

    fix(include_globals): correct import behaviour

--- src/includes/include_globals.php
+++ src/includes/include_globals.php
@@ -18,25 +18,12 @@ namespace pgform\embedded {
     }
 
     /**
-     * Return the relative url of a file from the plugin's directory.
-     * @method
-     * @param string $path A filename to check the relative url for.
-     * @return string
-     */
-    function relative_dir_url ($path) {
-        if (function_exists('plugins_url') && function_exists('plugin_dir_path')) {
-            $path = dirname($path);
-        }
-        return relative_url($path) . "/";
-    }
-
-    /**
      * Return the relative url of a file from the plugin's base directory.
      * @method
      * @param string $path A filename to check the relative url for.
      * @return string
      */
-    function relative_file_url_from_base ($path) {
+    function relative_file_url ($path) {
         if (function_exists('plugins_url') && function_exists('plugin_dir_path')) {
             $plugin_dir = plugin_dir_path( dirname(__FILE__) );
             $path = $plugin_dir . "/src/" . $path;
@@ -50,7 +37,7 @@ namespace pgform\embedded {
      * @param string $path A filename to check the relative url for.
      * @return string
      */
-    function relative_dir_url_from_base ($path) {
+    function relative_dir_url ($path) {
         if (function_exists('plugins_url') && function_exists('plugin_dir_path')) {
             $plugin_dir = plugin_dir_path( dirname(__FILE__) );
             $path = $plugin_dir . "/src/" . $path;
@@ -58,8 +45,22 @@ namespace pgform\embedded {
         return relative_url($path) . "/";
     }
 
-    if (function_exists("wp_enqueue_script") && function_exists("wp_enqueue_style")) {
-        wp_enqueue_style("PGForm_globals", relative_file_url_from_base("includes/styles/global.css"));
-        wp_enqueue_script("PGForm_globals", relative_file_url_from_base("includes/scripts/global.js"));
+    function add_included_styles () {
+        if (function_exists("wp_enqueue_style")) {
+            wp_enqueue_script("PGForm_globals", relative_file_url("includes/scripts/global.js"));
+        }
+    }
+
+    function add_included_scripts () {
+        if (function_exists("wp_enqueue_script")) {
+            wp_enqueue_script("PGForm_globals", relative_file_url("includes/scripts/global.js"));
+        }
+    }
+
+
+
+    if (function_exists("add_action")) {
+        add_action("wp_enqueue_scripts", "\\pgform\\add_included_styles");
+        add_action("wp_enqueue_scripts", "\\pgform\\add_included_styles");
     }
 }
